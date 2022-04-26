4f0798420ec2e202d53a1756b1e196b0502ee99a
Denes <jose.araujo@zup.com.br>
2022-04-26 16:13:05 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/carrosunicos/CarrosunicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.carrosunicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosunicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosunicosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/zup/edu/carrosunicos/model/Carro.java
@@ -0,0 +1,5 @@
+package br.com.zup.edu.carrosunicos.model;
+
+public class Carro {
+
+}
